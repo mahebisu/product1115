aa1e89065b4e0674b8c12ad588310b94374063a5
88432611+mahebisu@users.noreply.github.com <88432611+mahebisu@users.noreply.github.com>
2021-12-14 18:13:35 +0900

    業者側の登録も完了

--- createproduct1115/src/components/GyoshaReg/GyoshaReg.js
+++ createproduct1115/src/components/GyoshaReg/GyoshaReg.js
@@ -1,69 +1,134 @@
+import React,{useState,useEffect} from 'react'
+
 import { Button, Container, Stack,Card,CardContent,CardMedia,Typography,TextField } from '@mui/material'
-import React,{useState} from 'react'
 import ResponsiveAppBarGyosha from '../Appbar/ResponsiveAppBarGyosha'
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
-
 // ログイン機能（講義からコピペ）
     import { auth } from "../../firebase";
     import {
-    onAuthStateChanged,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
+        onAuthStateChanged,
+        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword,
     } from "firebase/auth";
 
-const UserReg = () => {
+// firebaseのデータベース関連
+    import { collection, query, onSnapshot, addDoc,serverTimestamp } from "firebase/firestore";
+    import { db} from "../../firebase";
+
+
+const GyoshaReg = () => {
 
     const [IsLogin, setIsLogin] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [EmailGyosha, setEmailGyosha] = useState("");
+    const [PassGyosha, setPassGyosha] = useState("");
+
+    // useStateでfirebaseから読み込む、dataをデフォルトで定義  
+        const [data, setData] = useState(
+            [{
+                EmailGyosha: "",
+                PassGyosha: "",
+            }]
+        );
+
+    // useEffectでquery,collectionからデータを読み込む
+        useEffect(() => {
+            // query=コレクション（firebaseの箱のこと）の指定をする
+            const q = query(collection(db,"gyosha"));
+
+            const unsub = onSnapshot(q, (querySnapshot) => {
+
+                setData(
+                    querySnapshot.docs.map((doc) => (
+                        {
+                            EmailGyosha: doc.data().EmailGyosha,
+                            PassGyosha: doc.data().PassGyosha,
+                        }
+                    ))
+                );
+
+            });
+            
+            return () => unsub();
+
+        }, [])
 
 
     // useformはreact-hook-formのコンポーネント、分割代入してる
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid, isDirty, isSubmitting, touchedFields, submitCount },
-        getValues,
-        watch,
-        reset
-    } = useForm();
-
+        const {
+            register,
+            handleSubmit,
+            formState: { errors, isValid, isDirty, isSubmitting, touchedFields, submitCount },
+            getValues,
+            watch,
+            reset
+        } = useForm();
 
     // navigateを宣言
     let navigate = useNavigate();
 
+
     // フォーム送信時の処理
-    const onSubmit = (data) => {
-        // バリデーションチェックOK！なときに行う処理を追加
-        console.log("isDirty>",isDirty);
-        console.log("isValid>",isValid);
-        console.log(submitCount);
+    const onSubmit = () => {
+        // バリデーションチェックで行う処理⇒今後対応
+            console.log("isDirty>",isDirty);
+            console.log("isValid>",isValid);
+            console.log(submitCount);
+            console.log("メールとパスは>",EmailGyosha,PassGyosha);
+        // errorsを表示させる
+            console.log("errors>",errors);
 
-        // getValuesの値がエラーなく入れられているかのチェック、inValid?
+        // formで入力した値をgetValuesで取得する
+            let getValuetachi = getValues();
+            console.log("getValuetachi>",getValuetachi);
+
+        // ログインのfunctionを定義
+            async function SigninFunction() {
+                try {
+                    //Firebase ver9 compliant (modular)
+                    // authにemailとpassを確認
+                    await signInWithEmailAndPassword(auth, EmailGyosha, PassGyosha);
+
+                    navigate("/NyusatsuIchiran");
+                } catch (error) {
+                    alert(error.message);
+                }
+            };
+
+        // User登録のfunctionを定義
+            async function CreateUserFunction() {
+                try {
+                    //Firebase ver9 compliant (modular)
+                    // authにemailとpassを登録
+                        await createUserWithEmailAndPassword(auth, EmailGyosha, PassGyosha);
+                    
+                    //Firebase ver9 compliant
+                    // firebaseに新規でデータ追加する
+                        addDoc(collection(db, "gyosha"), {
+                            EmailGyosha: getValuetachi.EmailGyosha,
+                            PassGyosha: getValuetachi.PassGyosha,
+                            RegTimestamp: serverTimestamp(),
+                        });
+
+                    navigate("/GyoshaReg2");
+                } catch (error) {
+                    alert(error.message);
+                }
+            }
+
 
         
-        // getValuesの値をそれぞれfirebaseに送る工程
-
-
-
-        // 次のページに遷移する
-        navigate("/GyoshaReg2");
-        
-
-
+        // authでcreateUserWithEmailAndPasswordをする
+        // 同時にdbにuser情報を登録する
+            if(IsLogin){
+                SigninFunction();
+            }else{
+                CreateUserFunction();
+            };
 
     };
-
-
-    // formで入力した値をgetValuesで取得する
-        const getValuetachi = getValues();
-        console.log(getValuetachi);
-
-    // errorsを表示させる
-        console.log("errors>",errors);
 
 
 
@@ -76,14 +141,13 @@ const UserReg = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
 
                 <Container maxWidth="md" sx={{ pt: 5 }}>
-
                     
                     <Stack spacing={5}>
 
                         <Typography variant="h4" color="text.success"
                             sx={{textAlign:"center"}}
                         >
-                            新規業者登録
+                            新規業者エージェント登録
                         </Typography>
 
                         <TextField
@@ -96,23 +160,21 @@ const UserReg = () => {
                             })}
                             error={"EmailGyosha" in errors}
 
-                            onChange={(e) => setEmail(e.target.value)}
-
+                            onChange={(e) => setEmailGyosha(e.target.value)}
                         />
 
 
                         <TextField
-                            label="パスワードを設定(6文字以上)"
+                            label='パスワードを設定(6文字以上)'
                             type="password"
                             {...register("PassGyosha", {
                                 required: true,
                                 minLength: 6,
-                                message: "設定するパスワードを6文字以上で入力してください"
+                                message: "希望の紹介料（円）を入力してください"
                             })}
                             error={"PassGyosha" in errors}
 
-                            onChange={(e) => setPassword(e.target.value)}
-
+                            onChange={(e) => setPassGyosha(e.target.value)}
                         />
 
                         <hr/>
@@ -124,50 +186,38 @@ const UserReg = () => {
                             type="submit"
                             fullWidth
                             style={{margintop:500}}
-
-                            onClick={
-                                IsLogin
-                                  ? async () => {
-                                      try {
-                                        //Firebase ver9 compliant (modular)
-                                        await signInWithEmailAndPassword(auth, email, password);
-                                        navigate("/ProjectIchiran");
-                                      } catch (error) {
-                                        alert(error.message);
-                                      }
-                                    }
-                                  : async () => {
-                                      try {
-                                        //Firebase ver9 compliant (modular)
-                                        await createUserWithEmailAndPassword(auth, email, password);
-                                        navigate("/GyoshaReg2");
-                                      } catch (error) {
-                                        alert(error.message);
-                                      }
-                                    }
-                              }
-
                         >
-                                業者登録
+                            新規ユーザー登録
+
                         </Button>
 
                     </Stack>
-                    
 
                 </Container>
 
             </form>
 
-            <footer>
-                <div  sx={{margin:"1000 auto 1000"
-                        }}>
-                    footer
-                </div>
-            </footer>
+            <section style={{
+                display: 'flex',
+                justifyContent: 'space-between,center',
+                width: '95%',
+                margin: 'auto'
+            }}>
+                {/* dataをmapで表示 */}
+                {data.map((item,index) => (
+                    // 多要素は１つのdiv等で囲う
+                    <div key={index}>
+                        <div>{index}</div>
+                        <div>{item.EmailGyosha}</div>
+                        <div>{item.PassGyosha}</div>
+                    </div>
+                ))}
+            </section>
+
 
         </div>
 
     )
 }
 
-export default UserReg
+export default GyoshaReg
--- createproduct1115/src/components/GyoshaReg/GyoshaReg2.js
+++ createproduct1115/src/components/GyoshaReg/GyoshaReg2.js
@@ -1,5 +1,5 @@
 import { Button, Container, Stack,Card,CardContent,CardMedia,Typography,TextField } from '@mui/material'
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import ResponsiveAppBarGyosha from '../Appbar/ResponsiveAppBarGyosha'
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -12,42 +12,173 @@ import { useNavigate } from "react-router-dom";
     import FormControl from '@mui/material/FormControl';
     import FormLabel from '@mui/material/FormLabel';
 
+// ログイン機能（講義からコピペ）
+    import { auth } from "../../firebase";
+    import {
+        onAuthStateChanged,
+        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword,
+    } from "firebase/auth";
 
-// 写真をインポート
+// firebaseのデータベース関連
+    import { collection, query, onSnapshot, addDoc, setDoc, serverTimestamp,orderBy,doc } from "firebase/firestore";
+    import { db} from "../../firebase";
 
-const UserReg = () => {
 
-    // useformはreact-hook-formのコンポーネント、分割代入してる
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid, isDirty, isSubmitting, touchedFields, submitCount },
-        getValues,
-        watch,
-        reset
-    } = useForm();
+const GyoshaReg2 = () => {
+
+    const [IsLogin, setIsLogin] = useState(false);
+    const [EmailGyosha, setEmailGyosha] = useState("");
+    const [PassGyosha, setPassGyosha] = useState("");
+    const [GyoshaId, setGyoshaId] = useState("");
+    console.log("GyoshaId>",GyoshaId);
+    console.log("EmailGyosha>",EmailGyosha);
+
+    // useStateでfirebaseから読み込む、dataをデフォルトで定義  
+        const [data, setData] = useState(
+            [{
+                EmailGyosha: "",
+                PassGyosha: "",
+                NameGyosha: "",
+                AddressGyosha: "",
+                PhoneGyosha: "",
+                GyoshaBirthYear: "",
+                GyoshaBirthMonth: "",
+                GyoshaBirthDay: "",
+                NameGyoshaCompany: "",
+                AddressGyoshaCompany: "",
+                Gyoshashurui:"Baishuu",
+                RegTimestamp:"",
+                UpdateTimestamp: ""
+            }]
+        );
+
+    // useEffectを使ってデータを取得する
+        useEffect(() => {
+
+            // まずログインしているuserのメールアドレスを取得する
+            //Firebase ver9 compliant (modular)
+                const unSub1 = onAuthStateChanged(auth, (user) => {
+
+                    console.log("user情報>",user.email);
+                    // authにuser情報があれば、IsLoginをtrue
+                    user.email && setEmailGyosha(user.email);
+
+                });
+
+            return () => {
+                unSub1();
+            };
+        }, []);
+
+        // useEffectを使ってデータを取得する
+        useEffect(() => {
+
+            // 次にデータを取得して、メールアドレスに対応するdoc.idを取得する
+            //Firebase ver9 compliant (modular)
+                const q = query(collection(db, "gyosha"), orderBy("RegTimestamp", "desc"));
+                const unSub2 = onSnapshot(q, (snapshot) => {
+
+                    setData(
+                        snapshot.docs.map((doc,index) => {
+
+                            console.log(index,doc.data().EmailGyosha,EmailGyosha);
+
+                            if(doc.data().EmailGyosha === EmailGyosha){
+                                setGyoshaId(doc.id);
+                                console.log("ログイン中のidをGyoshaIdにsetした",doc.id);
+                            };
+
+                            return ({id: doc.id,
+                                EmailGyosha: doc.data().EmailGyosha,
+                                PassGyosha: doc.data().PassGyosha,
+                                NameGyosha: doc.data().NameGyosha,
+                                AddressGyosha: doc.data().AddressGyosha,
+                                PhoneGyosha: doc.data().PhoneGyosha,
+                                GyoshaBirthYear: doc.data().GyoshaBirthYear,
+                                GyoshaBirthMonth: doc.data().GyoshaBirthMonth,
+                                GyoshaBirthDay: doc.data().GyoshaBirthDay,
+                                NameGyoshaCompany: doc.data().NameGyoshaCompany,
+                                AddressGyoshaCompany: doc.data().AddressGyoshaCompany,
+                                Gyoshashurui:doc.data().Gyoshashurui,                
+                                RegTimestamp: doc.data().RegTimestamp,
+                                UpdateTimestamp: doc.data().UpdateTimestamp
+                            })
+                        })
+                    );
+
+                });
+
+            return () => {
+                unSub2();
+            };
+        }, [EmailGyosha]);
+
+
 
 
     // navigateを宣言
     let navigate = useNavigate();
 
+    // useformはreact-hook-formのコンポーネント、分割代入してる
+        const {
+            register,
+            handleSubmit,
+            formState: { errors, isValid, isDirty, isSubmitting, touchedFields, submitCount },
+            getValues,
+            watch,
+            reset
+        } = useForm();
+    
+
     // フォーム送信時の処理
-    const onSubmit = (data) => {
-        // バリデーションチェックOK！なときに行う処理を追加
-        console.log("isDirty>",isDirty);
-        console.log("isValid>",isValid);
-        console.log(submitCount);
+    const onSubmit = () => {
+        // バリデーションチェックで行う処理⇒今後対応
+            console.log("isDirty>",isDirty);
+            console.log("isValid>",isValid);
+            console.log(submitCount);
+            console.log("メールとパスは>",EmailGyosha,PassGyosha);
+        // errorsを表示させる
+            console.log("errors>",errors);
 
-        // getValuesの値がエラーなく入れられているかのチェック、inValid?
+        // formで入力した値をgetValuesで取得する
+            let getValuetachi = getValues();
+            console.log("getValuetachi>",getValuetachi);
 
-        
-        // getValuesの値をそれぞれfirebaseに送る工程
+        // User登録のfunctionを定義
+            async function CreateGyoshaFunction() {
+                try {
+
+                    //Firebase ver9 compliant
+                    // firebaseのログイン中のuserデータにデータ追加する
+                        setDoc(
+                            doc(collection(db, "gyosha"), GyoshaId),
+                         {
+                            NameGyosha: getValuetachi.NameGyosha,
+                            AddressGyosha: getValuetachi.AddressGyosha,
+                            PhoneGyosha: getValuetachi.PhoneGyosha,
+                            GyoshaBirthYear: getValuetachi.GyoshaBirthYear,
+                            GyoshaBirthMonth: getValuetachi.GyoshaBirthMonth,
+                            GyoshaBirthDay: getValuetachi.GyoshaBirthDay,
+                            NameGyoshaCompany: getValuetachi.NameGyoshaCompany,
+                            AddressGyoshaCompany: getValuetachi.AddressGyoshaCompany,
+                            Gyoshashurui: Gyoshashurui,             
+                            UpdateTimestamp: serverTimestamp()
+                        },
+                        { merge: true }
+                        );
+
+                    navigate("/ProjectIchiran");
 
 
 
-        // 次のページに遷移する
-        navigate("/ProjectIchiran");
 
+                } catch (error) {
+                    alert(error.message);
+                }
+            }
+
+            CreateGyoshaFunction();
 
     };
 
@@ -59,12 +190,6 @@ const UserReg = () => {
             console.log(event.target.value)
         };
 
-    // formで入力した値をgetValuesで取得する
-        const getValuetachi = getValues();
-        console.log(getValuetachi);
-
-    // errorsを表示させる
-        console.log("errors>",errors);
 
 
 
@@ -96,33 +221,33 @@ const UserReg = () => {
                         <TextField
                             label="あなたの氏名"
                             type="text"
-                            {...register("NameNakoudo", {
+                            {...register("NameGyosha", {
                             required: true,
                             message: "氏名を入力してください"
                             })}
-                            error={"NameNakoudo" in errors}
+                            error={"NameGyosha" in errors}
                         />
 
                         <TextField
                             label="あなたの住所"
                             type="text"
-                            {...register("AddressNakoudo", {
+                            {...register("AddressGyosha", {
                             required: true,
                             message: "あなたの住所を入力してください"
                             })}
-                            error={"AddressNakoudo" in errors}
+                            error={"AddressGyosha" in errors}
                         />
 
                         <TextField
                             label="あなたの電話番号"
                             type="number"
-                            {...register("PhoneNakoudo", {
+                            {...register("PhoneGyosha", {
                                 required: true,
                                 maxLength: 11,
                                 minLength: 11,
                                 message: "電話番号を11桁で入力してください"
                             })}
-                            error={"PhoneNakoudo" in errors}
+                            error={"PhoneGyosha" in errors}
                         />
 
                         <div>
@@ -134,7 +259,7 @@ const UserReg = () => {
                                 <TextField
                                     label="西暦〇年"
                                     type="number"
-                                    {...register("NakoudoBirthYear", {
+                                    {...register("GyoshaBirthYear", {
                                         required: true,
                                         maxLength: 4,
                                         minLength: 4,
@@ -146,7 +271,7 @@ const UserReg = () => {
                                 <TextField
                                     label="〇月"
                                     type="number"
-                                    {...register("NakoudoBirthMonth", {
+                                    {...register("GyoshaBirthMonth", {
                                         required: true,
                                         maxLength: 2,
                                         minLength: 1,
@@ -159,7 +284,7 @@ const UserReg = () => {
                                 <TextField
                                     label="〇日"
                                     type="number"
-                                    {...register("NakoudoBirthDay", {
+                                    {...register("GyoshaBirthDay", {
                                         required: true,
                                         maxLength: 2,
                                         minLength: 1,
@@ -175,21 +300,21 @@ const UserReg = () => {
                         <TextField
                             label="所属する会社名"
                             type="text"
-                            {...register("NameNakoudo", {
+                            {...register("NameGyoshaCompany", {
                             required: true,
                             message: "所属する会社名を入力してください"
                             })}
-                            error={"NameNakoudo" in errors}
+                            error={"NameGyoshaCompany" in errors}
                         />
 
                         <TextField
                             label="会社の住所"
                             type="text"
-                            {...register("AddressNakoudo", {
+                            {...register("AddressGyoshaCompany", {
                             required: true,
                             message: "会社の住所を入力してください"
                             })}
-                            error={"AddressNakoudo" in errors}
+                            error={"AddressGyoshaCompany" in errors}
                         />
 
                         {/* 物件の種類を選択するラジオボタンをつくる */}
@@ -230,16 +355,33 @@ const UserReg = () => {
 
             </form>
 
-            <footer>
-                <div  sx={{margin:"1000 auto 1000"
-                        }}>
-                    footer
-                </div>
-            </footer>
+            <section style={{
+                display: 'flex',
+                justifyContent: 'space-between,center',
+                width: '95%',
+                margin: 'auto'
+            }}>
+                {/* dataをmapで表示 */}
+                {data.map((item,index) => (
+                    // 多要素は１つのdiv等で囲う
+                    <div key={index}>
+                        <div>{index}</div>
+                        <div>{item.id}</div>
+                        <div>{item.EmailGyosha}</div>
+                        <div>{item.PassGyosha}</div>
+                        <div>{item.NameGyosha}</div>
+                        <div>{item.AddressGyosha}</div>
+                        <div>{item.PhoneGyosha}</div>
+                        <div>{item.GyoshaBirthYear}</div>
+                        <div>{item.GyoshaBirthMonth}</div>
+                        <div>{item.GyoshaBirthDay}</div>
+                    </div>
+                ))}
+            </section>
 
         </div>
 
     )
 }
 
-export default UserReg
+export default GyoshaReg2
