4a5a1d697fe9328ea0687c0684702b5f7c56cd1b
88432611+mahebisu@users.noreply.github.com <88432611+mahebisu@users.noreply.github.com>
2021-12-14 12:19:03 +0900

    authとuserdbを連携できた

--- createproduct1115/src/components/UserReg/UserReg2.js
+++ createproduct1115/src/components/UserReg/UserReg2.js
@@ -1,51 +1,187 @@
+import React,{useState,useEffect} from 'react'
 import { Button, Container, Stack,Card,CardContent,CardMedia,Typography,TextField } from '@mui/material'
-import React,{useState} from 'react'
 import ResponsiveAppBar from '../Appbar/ResponsiveAppBar'
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
-// 写真をインポート
+// ログイン機能（講義からコピペ）
+    import { auth } from "../../firebase";
+    import {
+        onAuthStateChanged,
+        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword,
+    } from "firebase/auth";
 
-const UserReg = () => {
+// firebaseのデータベース関連
+    import { collection, query, onSnapshot, addDoc, setDoc, serverTimestamp,orderBy,doc } from "firebase/firestore";
+    import { db} from "../../firebase";
+
+const UserReg2 = () => {
+
+    const [IsLogin, setIsLogin] = useState(false);
+    const [EmailNakoudo, setEmailNakoudo] = useState("");
+    const [PassNakoudo, setPassNakoudo] = useState("");
+    const [NakoudoId, setNakoudoId] = useState("");
+    console.log("NakoudoId>",NakoudoId);
+    console.log("EmailNakoudo>",EmailNakoudo);
+
+    //   ここがログインできてるかどうか処理
+        useEffect(() => {
+            //Firebase ver9 compliant (modular)
+            const unSub = onAuthStateChanged(auth, (user) => {
+                console.log("user情報>",user.email);
+                // authにuser情報があれば、IsLoginをtrue
+                user.email && setEmailNakoudo(user.email);
+                });
+            return () => unSub();
+        });
+
+
+    // useStateでfirebaseから読み込む、dataをデフォルトで定義  
+        const [data, setData] = useState(
+            [{
+                id:"",
+                EmailNakoudo: "",
+                PassNakoudo: "",
+                NameNakoudo: "",
+                AddressNakoudo: "",
+                PhoneNakoudo: "",
+                NakoudoBirthYear: "",
+                NakoudoBirthMonth: "",
+                NakoudoBirthDay: "",
+                RegTimestamp:"",
+                UpdateTimestamp: ""
+            }]
+        );
+
+    // useEffectを使ってデータを取得する
+        useEffect(() => {
+
+            // まずログインしているuserのメールアドレスを取得する
+            //Firebase ver9 compliant (modular)
+                const unSub1 = onAuthStateChanged(auth, (user) => {
+
+                    new Promise((resolve) => {//Promise型を返す
+
+                        function procedure() {
+
+                            console.log("user情報>",user.email);
+                            // authにuser情報があれば、IsLoginをtrue
+                            user.email && setEmailNakoudo(user.email);
+                            resolve();
+                            
+                        }
+                    
+                        setTimeout(procedure, 1000);
+                    
+                    });
+
+                });
+
+            // 次にデータを取得して、メールアドレスに対応するdoc.idを取得する
+            //Firebase ver9 compliant (modular)
+                const q = query(collection(db, "user"), orderBy("RegTimestamp", "desc"));
+                const unSub2 = onSnapshot(q, (snapshot) => {
+
+                    setData(
+                        snapshot.docs.map((doc,index) => {
+
+                            console.log(index,doc.data().EmailNakoudo,EmailNakoudo);
+
+                            if(doc.data().EmailNakoudo === EmailNakoudo){
+                                setNakoudoId(doc.id);
+                                console.log("ログイン中のidをNakoudoIdにsetした",doc.id);
+                            };
+
+                            return ({id: doc.id,
+                                EmailNakoudo: doc.data().EmailNakoudo,
+                                PassNakoudo: doc.data().PassNakoudo,
+                                NameNakoudo: doc.data().NameNakoudo,
+                                AddressNakoudo: doc.data().AddressNakoudo,
+                                PhoneNakoudo: doc.data().PhoneNakoudo,
+                                NakoudoBirthYear: doc.data().NakoudoBirthYear,
+                                NakoudoBirthMonth: doc.data().NakoudoBirthMonth,
+                                NakoudoBirthDay: doc.data().NakoudoBirthDay,
+                                RegTimestamp: doc.data().RegTimestamp,
+                            })
+                        })
+                    );
+
+                });
+
+                async function unSub3() {
+                    await unSub1();
+                    unSub2();
+                }
+
+            return () => {
+                unSub3();
+            };
+        }, []);
+
 
     // useformはreact-hook-formのコンポーネント、分割代入してる
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid, isDirty, isSubmitting, touchedFields, submitCount },
-        getValues,
-        watch,
-        reset
-    } = useForm();
+        const {
+            register,
+            handleSubmit,
+            formState: { errors, isValid, isDirty, isSubmitting, touchedFields, submitCount },
+            getValues,
+            watch,
+            reset
+        } = useForm();
+
+    // navigateを宣言
+        let navigate = useNavigate();
+
 
     // フォーム送信時の処理
-    const onSubmit = (data) => {
-        // バリデーションチェックOK！なときに行う処理を追加
-        console.log("isDirty>",isDirty);
-        console.log("isValid>",isValid);
-        console.log(submitCount);
+        const onSubmit = () => {
+            // バリデーションチェックで行う処理⇒今後対応
+                console.log("isDirty>",isDirty);
+                console.log("isValid>",isValid);
+                console.log(submitCount);
+                console.log("メールとパスは>",EmailNakoudo,PassNakoudo);
+            // errorsを表示させる
+                console.log("errors>",errors);
 
-        // getValuesの値がエラーなく入れられているかのチェック、inValid?
+            // formで入力した値をgetValuesで取得する
+                let getValuetachi = getValues();
+                console.log("getValuetachi>",getValuetachi);
 
-        
-        // getValuesの値をそれぞれfirebaseに送る工程
+            // User登録のfunctionを定義
+                async function CreateUserFunction2() {
+                    try {
+
+                        //Firebase ver9 compliant
+                        // firebaseのログイン中のuserデータにデータ追加する
+                            setDoc(
+                                doc(collection(db, "user"), NakoudoId),
+                             {
+                                NameNakoudo: getValuetachi.NameNakoudo,
+                                AddressNakoudo: getValuetachi.AddressNakoudo,
+                                PhoneNakoudo: getValuetachi.PhoneNakoudo,
+                                NakoudoBirthYear: getValuetachi.NakoudoBirthYear,
+                                NakoudoBirthMonth: getValuetachi.NakoudoBirthMonth,
+                                NakoudoBirthDay: getValuetachi.NakoudoBirthDay,
+                                UpdateTimestamp: serverTimestamp(),
+                            },
+                            { merge: true }
+                            );
+
+                        navigate("/NyusatsuIchiran");
 
 
 
-        // 次のページに遷移する
 
+                    } catch (error) {
+                        alert(error.message);
+                    }
+                }
 
+                CreateUserFunction2();
 
-    };
-
-
-    // formで入力した値をgetValuesで取得する
-        const getValuetachi = getValues();
-        console.log(getValuetachi);
-
-    // errorsを表示させる
-        console.log("errors>",errors);
-
+        };
 
 
     return (
@@ -173,16 +309,34 @@ const UserReg = () => {
 
             </form>
 
-            <footer>
-                <div  sx={{margin:"1000 auto 1000"
-                        }}>
-                    footer
-                </div>
-            </footer>
+            <section style={{
+                display: 'flex',
+                justifyContent: 'space-between,center',
+                width: '95%',
+                margin: 'auto'
+            }}>
+                {/* dataをmapで表示 */}
+                {data.map((item,index) => (
+                    // 多要素は１つのdiv等で囲う
+                    <div key={index}>
+                        <div>{index}</div>
+                        <div>{item.id}</div>
+                        <div>{item.EmailNakoudo}</div>
+                        <div>{item.PassNakoudo}</div>
+                        <div>{item.NameNakoudo}</div>
+                        <div>{item.AddressNakoudo}</div>
+                        <div>{item.PhoneNakoudo}</div>
+                        <div>{item.NakoudoBirthYear}</div>
+                        <div>{item.NakoudoBirthMonth}</div>
+                        <div>{item.NakoudoBirthDay}</div>
+                    </div>
+                ))}
+            </section>
+
 
         </div>
 
     )
 }
 
-export default UserReg
+export default UserReg2
